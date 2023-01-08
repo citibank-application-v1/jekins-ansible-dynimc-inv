3b056774eeb4084c17cce02ce9094804ca0d642b
Srirama K S <54227930+srirama16@users.noreply.github.com>
2023-01-08 15:51:46 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,13 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+   region  = "ap-south-1" 
 }
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
-     ami = "ami-010d564ad44bd1912"
+     ami = "ami-042e255d794504f4d"
      instance_type = "t2.micro"
-     security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     security_groups = ["launch-wizard-12"]
+     key_name        = "devopstraining"
      tags = {
         Name = "tomcatservers"
      }
